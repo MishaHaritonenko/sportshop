bb61bac08d63d76cd6741a50ddeca9c3ee271897
MishaHaritonenko <107568538+MishaHaritonenko@users.noreply.github.com>
2022-10-17 13:51:58 +0400

    Delete sportshop/main directory

--- sportshop/main/INFORMATION_ABOUT_REGISTRED_BAER.sql
+++
@@ -1,8 +0,0 @@
-create table INFORMATION_ABOUT_REGISTRED_BAER
-(
-    id         INTEGER
-        primary key,
-    FIRST_NAME TEXT not null,
-    LAST_NAME  TEXT not null
-);
-
--- sportshop/main/INFORMATION_ABOUT_SALESMAN.sql
+++
@@ -1,8 +0,0 @@
-create table INFORMATION_ABOUT_SALESMAN
-(
-    id         INTEGER
-        primary key,
-    FIRST_NAME TEXT not null,
-    LAST_NAME  TEXT not null
-);
-
